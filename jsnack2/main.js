0ffdc1e916484072f6203755ad2cdb023fea90a8
Tolihama <fra070@gmail.com>
2021-11-08 13:25:39 +0100

    Snack 2

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -1,3 +1,11 @@
 /* Snack 2
 A partire da un array di numeri, genera un secondo array con le potenze al quadrato di ogni numero.
 Es: [1, 2, 3, 4, 5] => [1, 4, 9, 16, 25] */
+
+// Start Array
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+
+// New Array (square numbers)
+const squareNumbers = numbers.map(e => e * e);
+console.log(squareNumbers);
