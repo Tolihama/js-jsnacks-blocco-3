7dff52fb4b693b173feb72bd4cf7ba27975bed47
Tolihama <fra070@gmail.com>
2021-11-08 14:49:21 +0100

    Snack1 punto 4

--- jsnack1/main.js
+++ jsnack1/main.js
@@ -21,30 +21,33 @@
 - Età
 - Media punti fatti per partita
 - Percentuale di successo per tiri da 3 punti */
-const player = {
-    code: '',
-    firstName: 'Paul',
-    lastName: 'Red',
-    age: 22,
-    averageScorePerMatch: 22,
-    threePointSuccessRate: 100,
-};
 
-console.log('Init player obj', player);
+// Player Init Proprieties
+let code = '';
+let firstName = '';
+let lastName = '';
+let age = 0;
+let averageScorePerMatch = 0;
+let threePointSuccessRate = 0;
 
 /* 2. Generare tramite delle funzioni le statistiche di gioco, secondo queste regole:
 - il codice giocatore deve essere formato da 3 lettere maiuscole casuali e 3 cifre casuali
 - la media punti fatti per partita deve essere compresa tra 0 e 50
 - la percentuale di successo per tiri da 3 punti deve essere compresa tra 0 e 100 */
-player.code = codeGen();
-player.averageScorePerMatch = rand(0, 50);
-player.threePointSuccessRate = rand(0, 100);
-
-console.log('Generation player stat', player);
+// Player obj
+const playerOne = randPlayer();
 
 /* 3. Stampare Nome, cognome, età e codice giocatore. */
-console.log(player.firstName, player.lastName, player.age, player.code);
+console.log(playerOne.firstName, playerOne.lastName, playerOne.age, playerOne.code);
 
+/* 4. Creare un array di 10 giocatori di basket, con le regole sopra indicate */
+const playersList = [];
+
+for (let i = 0; i < 10; i++) {
+    playersList.push(randPlayer());
+}
+
+console.log('Lista giocatori completa:', playersList);
 
 /**
  * FUNCTIONS
@@ -64,7 +67,51 @@ function codeGen() {
     return code;
 }
 
+// Random firstName
+function randFirstName() {
+    const names = [
+        'Tim',
+        'Tom',
+        'James',
+        'Carl',
+        'Clark',
+        'Fred',
+        'Jerry'
+    ];
+    return names[rand(0, names.length - 1)];
+}
+
+// Random lastName
+function randLastName() {
+    const surnames = [
+        'Lebron',
+        'Pedretti'
+    ];
+    return surnames[rand(0, surnames.length - 1)];
+}
+
 // Random number generator
 function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Random player generator
+function randPlayer() {
+    code = codeGen();
+    firstName = randFirstName();
+    lastName = randLastName();
+    age = rand(15, 40);
+    averageScorePerMatch = rand(0, 50);
+    threePointSuccessRate = rand(0, 100);
+
+    const player = {
+        code,
+        firstName,
+        lastName,
+        age,
+        averageScorePerMatch,
+        threePointSuccessRate,
+    };
+
+    return player;
 }
